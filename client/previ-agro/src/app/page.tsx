1e2a9b29d9c3dbdd753e762b602ba529bbe22615
Erick Campuzano <89644016+erick-gct@users.noreply.github.com>
2025-07-04 11:59:35 -0500

    uuuuUpdate page.tsx

--- client/previ-agro/src/app/page.tsx
+++ client/previ-agro/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       if (user) {
-        router.replace("/(protected)");
+        router.replace("/");
       } else {
         router.replace("/login");
       }
