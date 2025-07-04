7becd9f1a00206fb48c86ff4ed2e1b1d01167968
Erick Campuzano <89644016+erick-gct@users.noreply.github.com>
2025-07-04 09:44:02 -0500

    Update next.config.ts

--- client/previ-agro/next.config.ts
+++ client/previ-agro/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   eslint: {
+    // Ignora todos los errores de ESLint durante `next build`
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
