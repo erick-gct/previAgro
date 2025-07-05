64094d5e69d7feb8b16452c6b344aa9a40deb9ea
Erick Campuzano <89644016+erick-gct@users.noreply.github.com>
2025-07-05 17:20:22 -0500

    cambio final

--- client/previ-agro/src/app/redirect/page.tsx
+++ client/previ-agro/src/app/redirect/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/lib/firebase';
-import { LoadingModal } from './components/loading';
+import { LoadingModal } from '../components/loading';
 
 export default function RedirectPage() {
   const router = useRouter();
